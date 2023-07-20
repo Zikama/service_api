9fc79252b5c1e4edb0ecfe17d8cb2b7649a82296
Olaotan Lawal <olaotan104@gmail.com>
2023-07-20 22:10:43 +0100

    adds cors to service

--- service/index.js
+++ service/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const morgan = require('morgan')
-const {verifyUser, scheduleReminder } = require('./route')
+const cors = require('cors')
+const {verifyUser, scheduleReminder, handleMessages } = require('./route')
 const cookieParser = require('cookie-parser');
 require('dotenv').config(); 
 
@@ -19,6 +20,7 @@ app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({extended:false}));
 app.use(cookieParser());
+app.use(cors()); 
 
 
 app.use((req,res,next)=>{
@@ -27,4 +29,5 @@ app.use((req,res,next)=>{
 
 app.use('/verify',verifyUser)
 app.use('/schedule', scheduleReminder )
+app.use('/message', handleMessages)
 
